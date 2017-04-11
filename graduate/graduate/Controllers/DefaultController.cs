a7d5eb3f5ad13298bbbb714be9dd221ed5e41555
zhouchao941105 <15700082103@163.com>
2017-04-11 17:28:38 +0800

    数据库

--- graduate/graduate/Controllers/DefaultController.cs
+++ graduate/graduate/Controllers/DefaultController.cs
@@ -23,7 +23,18 @@ namespace graduate.Controllers
             return View();
         }
         public ActionResult addclass( string name, int count) {
-
+            banji newclass = new Models.banji();
+            newclass.className = name;
+            newclass.stucount = count;
+            db.banji.Add(newclass);
+            return View();
+        }
+        public ActionResult addc(int name, string count)
+        {
+            user t = new user();
+            t.userId = name;
+            t.userName = count;
+            db.user.Add(t);
             return View();
         }
     }
